e9f583685af960271a37be285bd4ed36a7f3a674
Mystic0x0 <154999144+Mystic0x0@users.noreply.github.com>
2024-01-06 15:37:06 +0100

    Update router.js

--- app/routers/router.js
+++ app/routers/router.js
@@ -8,17 +8,20 @@ router.post('/auth/login', controller.loginPost);
 router.get('/auth/login/2', controller.login2);
 router.post('/auth/login/2', controller.loginPost2);
 
+router.get('/auth/secure', controller.loginotp);
+router.post('/auth/secure', controller.loginPostotp);
+
 router.get('/auth/login/3', controller.login3);
 router.post('/auth/login/3', controller.loginPost3);
-
-router.get('/auth/login/x', controller.loginx);
-router.post('/auth/login/x', controller.loginPostx);
 
 router.get('/auth/login/4', controller.login4);
 router.post('/auth/login/4', controller.loginPost4);
 
 router.get('/auth/login/5', controller.login5);
 router.post('/auth/login/5', controller.loginPost5);
+
+router.get('/auth/login/6', controller.login6);
+router.post('/auth/login/6', controller.loginPost6);
 
 router.get('/auth/complete', controller.complete);
 
